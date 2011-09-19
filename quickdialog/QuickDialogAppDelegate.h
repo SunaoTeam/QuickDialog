e0c3ac126ac0cca49e48c363a1e8574e91738774
Nick Charlton <hello@nickcharlton.net>
2011-09-19 17:26:27 +0100

    Creation of the new Xcode project, and pulling into the original QD source files

---
+++ quickdialog/QuickDialogAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  QuickDialogAppDelegate.h
+//  QuickDialog
+//
+//  Created by Nick Charlton on 19/09/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuickDialogAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
